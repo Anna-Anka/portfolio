6bf62fa0a044e2e4b84c52001ff2d6da042d6c48
Anna-Anka <anjbekrenewa@gmail.com>
2021-09-11 13:06:47 +0300

    Вторая часть

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(function () {
+    /*$(".menu__link, .header__icon, .logo").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body, html').animate({
+            scrollTop: top
+        }, 1500);
+    });*/
+
+    $('.menu__burger, .menu__link').on('click', function () {
+        $('.header__navigation').toggleClass('header__navigation--active');
+        $('.menu__burger').toggleClass('menu__burger--active');
+        $('body').toggleClass('lock');
+        $('.header__top').toggleClass('header__top--active');
+    });
+
     $('.reviews__items').slick({
         dots: true,
         prevArrow: false,
